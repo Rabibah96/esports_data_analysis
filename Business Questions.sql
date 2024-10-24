cff7b2975cfe75c77b7c288f8d6a789a13cb1c4f
Rabibah Munir <135952028+Rabibah96@users.noreply.github.com>
2024-10-25 00:55:35 +0500

    Add files via upload

---
+++ Business Questions.sql
@@ -0,0 +1,12 @@
+--Business Questions
+
+--1. Which teams are the most successful in terms of total earnings?
+--2. Which countries have the most successful esports players based on prize money?
+--3. What is the average prize money earned by players in different genres?
+--4. What are the top-paying games for both teams and players?
+--5. Which continent has the highest representation in top esports competitions?
+--6. Which individual players are earning the most in a specific game e.g., PUBG?
+--7. Which teams perform better in high-stakes tournaments versus low-stakes tournaments?
+--8. What is the impact of tournament size (number of participants) on the total prize pool?
+--9. What are the most common career trajectories of top players (i.e., starting in one game and transitioning to others)?
+--10. Which genres show the highest volatility in prize pools, and what factors contribute to this volatility?
